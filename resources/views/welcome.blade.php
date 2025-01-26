cf929275ad806644835b0466e98082a955619413
Roman Artemyev <artemyev.ram@gmail.com>
2025-01-27 00:00:29 +0300

    Создание прижатого футера и копирайта

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,30 +6,47 @@
     <title>{{config('APP_NAME')}}</title>
     @vite('resources/css/app.css')
 </head>
+<body class="h-screen flex flex-col">
+
 <header class="py-2 bg-gray-900 text-white text-3xl font-bold">
     <div class="container">
         <span class="text-emerald-500">in</span><span>Spotlight</span>
     </div>
 </header>
-<main class="container">
-    <div>
-        <div>
-            <p>Lorem ipsum dolor sait amet, consectetur adipisicing elit. A aliquam amet, consequatur cum cumque distinctio dolor
-                eaque eius et eum facere fugiat iure maiores perspiciatis, quo ratione repudiandae saepe, sequi soluta suscipit
-                vel velit veniam. Aliquam architecto consectetur consequatur corporis culpa cum delectus dicta, dolorem, error
-                esse et eum expedita facilis fuga labore minima minus neque, odio optio quae quidem sed soluta tempora veritatis
-                voluptate? A accusamus, aliquid amet aspernatur at atque autem beatae blanditiis commodi dicta distinctio
-                dolorem dolorum ducimus eaque enim eos harum illo incidunt ipsa ipsum iste iusto libero maiores maxime molestiae
-                mollitia optio perspiciatis provident quas qui quia quidem quisquam recusandae repellat unde vitae voluptatem.
-                Aspernatur consectetur corporis cupiditate deserunt eaque earum error exercitationem facere fuga fugiat fugit
-                iste itaque laboriosam magni nam numquam obcaecati quia quidem, reiciendis, reprehenderit repudiandae sint
-                tempore totam veniam voluptas. Autem consectetur error esse excepturi exercitationem quia recusandae soluta
-                ullam vero?</p>
+<main class="flex-grow">
+    <div class="container">
+        <div class="w-1/2">
+            <p>Lorem ipsum dolor sait amet, consectetur adipisicing elit. A aliquam amet, consequatur cum cumque
+                distinctio dolor
+                eaque eius et eum facere fugiat iure maiores perspiciatis, quo ratione repudiandae saepe, sequi soluta
+                suscipit
+                vel velit veniam. Aliquam architecto consectetur consequatur corporis culpa cum delectus dicta, dolorem,
+                error
+                esse et eum expedita facilis fuga labore minima minus neque, odio optio quae quidem sed soluta tempora
+                veritatis
+                voluptate? A accusamus, aliquid amet aspernatur at atque autem beatae blanditiis commodi dicta
+                distinctio
+                dolorem dolorum ducimus eaque enim eos harum illo incidunt ipsa ipsum iste iusto libero maiores maxime
+                molestiae
+                mollitia optio perspiciatis provident quas qui quia quidem quisquam recusandae repellat unde vitae
+                voluptatem.
+                Aspernatur consectetur corporis cupiditate deserunt eaque earum error exercitationem facere fuga fugiat
+                fugit
+                iste itaque laboriosam magni nam numquam obcaecati quia quidem, reiciendis, reprehenderit repudiandae
+                sint
+                tempore totam veniam voluptas. Autem consectetur error esse excepturi exercitationem quia recusandae
+                soluta
+                ullam vero?
+
+            </p>
         </div>
         <div></div>
     </div>
-
 </main>
-<footer></footer>
+<footer class=" text-center py-2 bg-gray-900 text-white text-l">
+    <div class="container">
+        <span>&copy;{{ config('app.name') }} {{ date('Y') }}. All right reserved.</span>
+    </div>
+</footer>
 </body>
 </html>
