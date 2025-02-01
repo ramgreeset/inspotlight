bfc19da3cac261bada276d55c5b9bbd15c918422
Roman Artemyev <artemyev.ram@gmail.com>
2025-02-01 17:22:19 +0300

    Создание реквест классов для моделей

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Category\CategoryResource;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index() {
+        return CategoryResource::collection(Category::all());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{$category}',[CategoryController::class, 'show']);
+Route::post('/categories',[CategoryController::class, 'store']);
+Route::patch('/categories/{$category}',[CategoryController::class, 'update']);
+Route::delete('/categories/{$category}',[CategoryController::class, 'destroy']);
+
+
